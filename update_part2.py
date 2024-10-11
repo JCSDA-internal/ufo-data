b7ea4928c1bc8f9e7914c02d11306d19658ee545
mikecooke77 <michael.c.cooke@metoffice.gov.uk>
2024-10-11 17:26:52 +0100

    Remove condensed water

--- update_part2.py
+++ update_part2.py
@@ -80,28 +80,31 @@ if __name__ == '__main__':
     # -------------------------------------------------------------------------------
     workingDir = ["./"]
     # could also be specific_humidity_at_two_meters_above_surface
-    renameVariableInDataFile(workingDir, "specific_humidity", "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water")
-    renameVariableInDataFile(workingDir, "specific_humidity_at_two_meters_above_surface", "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_at_2m")
+    renameVariableInDataFile(workingDir, "specific_humidity", "water_vapor_mixing_ratio_wrt_moist_air")
+    renameVariableInDataFile(workingDir, "specific_humidity_at_two_meters_above_surface", "water_vapor_mixing_ratio_wrt_moist_air_at_2m")
     renameVariableInDataFile(workingDir, "specific_humidity_background_error", "water_vapor_mixing_ratio_wrt_moist_air_background_error")
+    renameVariableInDataFile(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water", "water_vapor_mixing_ratio_wrt_moist_air")
+    renameVariableInDataFile(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_at_2m", "specific_humidity_at_two_meters_above_surface")
+    renameVariableInDataFile(workingDir, "specific_humidity_at_two_meters_above_surface", "water_vapor_mixing_ratio_wrt_moist_air_at_2m")
+    renameVariableInDataFile(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_background_error", "water_vapor_mixing_ratio_wrt_moist_air_background_error")
 
-    # List of variables to remove condensed water from
-    files = ["gnssro_geoval_2018041500_1obs_bending_angle.nc4", "scatwind_geoval_2020121500_m.nc", "sfc_geoval_2020121500_m.nc", 
-             "sfcship_geoval_2020121500_m.nc", "gnssro_geoval_2018041500_3prof.nc4", "rass_tv_geoval_2020121500.nc",
-             "sondes_geovals_2021121200_m.nc4", "scatwind_geovals_2021121200_m.nc4",
-             "vadwind_geoval_2020121500.nc", "sfcship_geovals_2021121200_m.nc4", "pibal_geovals_2021121200_m.nc4",
-             "pibal_geoval_2021121200_m.nc4", "satwind_geovals_2021121200_m.nc4", "aircraft_geovals_2021121200_m.nc4",
-             "satwind_geoval_2020121500_m.nc", "sfc_geovals_2021121200_m.nc4", "sondes_geoval_2020121500_m.nc",
-             "aircraft_geoval_2020121500_m.nc", "gnssro_bend_geoval_2022061500_m.nc4", "gnssro_geoval_2018041500_m.nc4",
-             "gnssro_geoval_2018041500_s.nc4", "sondes_q_geoval_2020121500.nc4", "gnssro_geoval_2018041500_s_2d.nc4",
-             "aircraft_geoval_2018041500_m.nc4", "geovals_radar_mrms_202205122200.nc",
-             "gsisfc_tsen_geoval_2018041500_m.nc4", "gsisfc_uv_geoval_2018041500_m.nc4", "sondes_background_error_vert_interp_air_pressure_geoval_2018041500_s.nc4",
-             "sondes_background_error_vert_interp_height_geoval_2018041500_s.nc4", "geovals-mpas.mrms_reflectivity.20220216T000000Z_s.nc",
-             "aircraft_geoval_2018041500_s.nc4"]
-# groundgnss_geovals_20191230T0600Z.nc4
-    renameVariableInList(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water", "water_vapor_mixing_ratio_wrt_moist_air", files)
+#    # List of variables to remove condensed water from
+#    files = ["gnssro_geoval_2018041500_1obs_bending_angle.nc4", "scatwind_geoval_2020121500_m.nc", "sfc_geoval_2020121500_m.nc", 
+#             "sfcship_geoval_2020121500_m.nc", "gnssro_geoval_2018041500_3prof.nc4", "rass_tv_geoval_2020121500.nc",
+#             "sondes_geovals_2021121200_m.nc4", "scatwind_geovals_2021121200_m.nc4",
+#             "vadwind_geoval_2020121500.nc", "sfcship_geovals_2021121200_m.nc4", "pibal_geovals_2021121200_m.nc4",
+#             "pibal_geoval_2021121200_m.nc4", "satwind_geovals_2021121200_m.nc4", "aircraft_geovals_2021121200_m.nc4",
+#             "satwind_geoval_2020121500_m.nc", "sfc_geovals_2021121200_m.nc4", "sondes_geoval_2020121500_m.nc",
+#             "aircraft_geoval_2020121500_m.nc", "gnssro_bend_geoval_2022061500_m.nc4", "gnssro_geoval_2018041500_m.nc4",
+#             "gnssro_geoval_2018041500_s.nc4", "sondes_q_geoval_2020121500.nc4", "gnssro_geoval_2018041500_s_2d.nc4",
+#             "aircraft_geoval_2018041500_m.nc4", "geovals_radar_mrms_202205122200.nc",
+#             "gsisfc_tsen_geoval_2018041500_m.nc4", "gsisfc_uv_geoval_2018041500_m.nc4", "sondes_background_error_vert_interp_air_pressure_geoval_2018041500_s.nc4",
+#             "sondes_background_error_vert_interp_height_geoval_2018041500_s.nc4", "geovals-mpas.mrms_reflectivity.20220216T000000Z_s.nc",
+#             "aircraft_geoval_2018041500_s.nc4", "amsua_n19_geoval_2018041500_m_rttovcpp.nc4"]
+#    renameVariableInList(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water", "water_vapor_mixing_ratio_wrt_moist_air", files)
 #    files = ["sondes_background_error_vert_interp_height_obsdiag_2018041500_s.nc4"]
 #    renameVariableInList(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_background_error", "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_background_error", files)
-    files = ["sondes_background_error_vert_interp_air_pressure_obsdiag_2018041500_s.nc4", "sondes_background_error_vert_interp_height_obsdiag_2018041500_s.nc4"]
-    renameVariableInList(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_background_error", "water_vapor_mixing_ratio_wrt_moist_air_background_error", files)
+#    files = ["sondes_background_error_vert_interp_air_pressure_obsdiag_2018041500_s.nc4", "sondes_background_error_vert_interp_height_obsdiag_2018041500_s.nc4"]
+#    renameVariableInList(workingDir, "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water_background_error", "water_vapor_mixing_ratio_wrt_moist_air_background_error", files)
 
 
